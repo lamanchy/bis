4ce21ac3c1050a2eb711e3ba29d7303100c14802
Ondřej Lomič <ondrej.lomic@gmail.com>
2023-01-29 19:28:32 +0100

    chore: update import emails

--- backend/bis/management/commands/import_emails.py
+++ backend/bis/management/commands/import_emails.py
@@ -1,4 +1,5 @@
 import json
+from pathlib import Path
 
 from django.core.management.base import BaseCommand
 
@@ -20,7 +21,8 @@ class Command(BaseCommand):
             page += 1
 
             print(page)
-            result = send([], 'GET', 'lists/28/subscribers', params=dict(page=page, per_page=1000))
+            result = send([], 'GET', 'lists/28/subscribers', params=dict(page=page))
+            print(result)
             data += result['data']
             if not result['next_page_url']:
                 break
@@ -29,8 +31,16 @@ class Command(BaseCommand):
 
     @with_paused_user_str_signal
     def handle(self, *args, **options):
-        with open('data.json', 'r', encoding='utf-8') as f:
-            data = json.load(f)
+        path = Path('data.json')
+
+        if path.exists():
+            with open(path, 'r', encoding='utf-8') as f:
+                data = json.load(f)
+        else:
+            data = self.get_subscribers()
+
+            with open(path, 'w', encoding='ut-8') as f:
+                json.dump(data, f)
 
         bis_emails = set(User.objects.values_list('email', flat=True))
         data_emails = [u for u in data if '+' not in u['email']]
@@ -38,30 +48,30 @@ class Command(BaseCommand):
         data_emails = set([u['email'] for u in data_emails])
         print(len(bis_emails), len(data_emails), len(bis_emails.intersection(data_emails)),
               len(bis_emails - data_emails), len(data_emails - bis_emails))
-
-        q = []
-        for item in data:
-            if item['email'] in (data_emails - bis_emails):
-                q.append({
-                    'email': item['email'],
-                    'subscribed_at': item['subscribed_at'],
-                    'name': item['subscriber']['name'],
-                    'surname': item['subscriber']['surname'],
-                    'source': item['subscriber']['source'],
-                })
-
-        with open('ecomail_only.txt', 'w', encoding='utf-8') as f2:
-            json.dump(q, f2, indent=2, ensure_ascii=False)
-
-        users = User.objects.filter(email__in=bis_emails - data_emails)
-
-        users = UserExportSerializer.get_related(users)
-
-        writer = XLSXWriter(users.model._meta.verbose_name_plural)
-        writer.from_queryset(users, UserExportSerializer)
-        file = writer.get_file()
-        with open(file.name, 'rb') as f1:
-            with open('bis_only.xlsx', 'wb') as f2:
-                f2.write(f1.read())
-
-        # print(json.dumps(data[0], indent=2))
+        #
+        # q = []
+        # for item in data:
+        #     if item['email'] in (data_emails - bis_emails):
+        #         q.append({
+        #             'email': item['email'],
+        #             'subscribed_at': item['subscribed_at'],
+        #             'name': item['subscriber']['name'],
+        #             'surname': item['subscriber']['surname'],
+        #             'source': item['subscriber']['source'],
+        #         })
+        #
+        # with open('ecomail_only.txt', 'w', encoding='utf-8') as f2:
+        #     json.dump(q, f2, indent=2, ensure_ascii=False)
+        #
+        # users = User.objects.filter(email__in=bis_emails - data_emails)
+        #
+        # users = UserExportSerializer.get_related(users)
+        #
+        # writer = XLSXWriter(users.model._meta.verbose_name_plural)
+        # writer.from_queryset(users, UserExportSerializer)
+        # file = writer.get_file()
+        # with open(file.name, 'rb') as f1:
+        #     with open('bis_only.xlsx', 'wb') as f2:
+        #         f2.write(f1.read())
+        #
+        # # print(json.dumps(data[0], indent=2))
